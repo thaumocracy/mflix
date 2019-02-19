da33eb0e8e30dc741cdda20ffa7b569e9bd7e0f2
Winterstorm <thunpwnz@gmail.com>
2019-02-19 23:02:13 +0400

    Chapter 1 finished

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -60,13 +60,18 @@ export default class MoviesDAO {
       // Find movies matching the "countries" list, but only return the title
       // and _id. Do not put a limit in your own implementation, the limit
       // here is only included to avoid sending 46000 documents down the
-      // wire.
-      cursor = await movies.find().limit(1)
+      // wire.    try {
+    cursor = await movies.find({
+      countries: {$in: countries}}, {
+      projection: {
+        title: 1
+      }
+    })
+      // cursor = await movies.find().limit(1)
     } catch (e) {
       console.error(`Unable to issue find command, ${e}`)
       return []
     }
-
     return cursor.toArray()
   }
 
@@ -110,13 +115,14 @@ export default class MoviesDAO {
 
     Given an array of one or more genres, construct a query that searches
     MongoDB for movies with that genre.
-    */
+    */    
+
 
     const searchGenre = Array.isArray(genre) ? genre : Array(genre)
 
     // TODO Ticket: Text and Subfield Search
     // Construct a query that will search for the chosen genre.
-    const query = {}
+    const query = {genres: {$in: searchGenre}}
     const project = {}
     const sort = DEFAULT_SORT
 
